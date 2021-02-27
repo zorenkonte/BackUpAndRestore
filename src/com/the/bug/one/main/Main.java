f3fe3a23c542beceff5f00c7372de8d30acc4568
DarkMode : On <muffintoppings36@gmail.com>
2021-02-27 16:37:20 +0800

    Main method loggin

--- src/com/the/bug/one/main/Main.java
+++ src/com/the/bug/one/main/Main.java
@@ -1,5 +1,6 @@
 package com.the.bug.one.main;
 
+import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 
 import static com.the.bug.one.job.BackUpTask.startTask;
@@ -8,10 +9,16 @@ import static com.the.bug.one.util.Utility.loadProperties;
 
 public class Main {
 
+    public static final Logger LOGGER = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
         PropertyConfigurator.configure(Main.class.getClassLoader().getResourceAsStream("resources/log4j.properties")); //register log4j config props
+
+        LOGGER.info("Creating default directory");
         createDefaultDirectory(); //create default folder if not exist on startup
+        LOGGER.info("Loading properties");
         loadProperties(); //load configs
+        LOGGER.info("Starting cron job");
         startTask(); //start cron job
     }
 }
