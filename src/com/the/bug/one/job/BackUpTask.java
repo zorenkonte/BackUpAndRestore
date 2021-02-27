010bf56e014e2992317080807dbb02f679f32218
DarkMode : On <muffintoppings36@gmail.com>
2021-02-27 16:45:21 +0800

    Back up class logger

--- src/com/the/bug/one/job/BackUpTask.java
+++ src/com/the/bug/one/job/BackUpTask.java
@@ -1,6 +1,8 @@
 package com.the.bug.one.job;
 
 import com.the.bug.one.config.PropertyConfig;
+import com.the.bug.one.util.Utility;
+import org.apache.log4j.Logger;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
 
@@ -9,6 +11,7 @@ import static org.quartz.JobBuilder.newJob;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class BackUpTask {
+    public static final Logger LOGGER = Logger.getLogger(BackUpTask.class);
     private static final PropertyConfig CONFIG = PropertyConfig.getInstance();
 
     public static void startTask() {
@@ -24,8 +27,9 @@ public class BackUpTask {
                     .build();
             scheduler.scheduleJob(job, trigger);
             scheduler.start();
+            LOGGER.info("Cron job started");
         } catch (SchedulerException e) {
-            e.printStackTrace();
+            LOGGER.error(e.getMessage());
         }
     }
 }
