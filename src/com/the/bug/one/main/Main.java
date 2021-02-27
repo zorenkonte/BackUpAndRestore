8398776d5f0236ebfe3efac22a9d1542b31d1275
DarkMode : On <muffintoppings36@gmail.com>
2021-02-27 16:33:35 +0800

    Initialize log4j configuration

--- src/com/the/bug/one/main/Main.java
+++ src/com/the/bug/one/main/Main.java
@@ -1,5 +1,7 @@
 package com.the.bug.one.main;
 
+import org.apache.log4j.PropertyConfigurator;
+
 import static com.the.bug.one.job.BackUpTask.startTask;
 import static com.the.bug.one.util.Utility.createDefaultDirectory;
 import static com.the.bug.one.util.Utility.loadProperties;
@@ -7,6 +9,7 @@ import static com.the.bug.one.util.Utility.loadProperties;
 public class Main {
 
     public static void main(String[] args) {
+        PropertyConfigurator.configure(Main.class.getClassLoader().getResourceAsStream("resources/log4j.properties")); //register log4j config props
         createDefaultDirectory(); //create default folder if not exist on startup
         loadProperties(); //load configs
         startTask(); //start cron job
