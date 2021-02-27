0592b7c8c685442c200e941c0ebce7b40c5f2f8d
DarkMode : On <muffintoppings36@gmail.com>
2021-02-27 16:49:45 +0800

    Back up job logger

--- src/com/the/bug/one/job/BackUpJob.java
+++ src/com/the/bug/one/job/BackUpJob.java
@@ -2,16 +2,19 @@ package com.the.bug.one.job;
 
 import com.the.bug.one.backup.BackUp;
 import com.the.bug.one.config.PropertyConfig;
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 public class BackUpJob implements Job {
 
+    public static final Logger LOGGER = Logger.getLogger(BackUpJob.class);
     private static final PropertyConfig CONFIG = PropertyConfig.getInstance();
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
+        LOGGER.info("Executing back-up job");
         BackUp.performBackUp(CONFIG); //do backup
     }
 }
