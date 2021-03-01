e6d621ce56e70b27888d838500d87d18e9ac0a54
DarkMode : On <muffintoppings36@gmail.com>
2021-03-01 16:36:02 +0800

    Immediately fire job once at start up

--- src/com/the/bug/one/job/BackUpTask.java
+++ src/com/the/bug/one/job/BackUpTask.java
@@ -1,8 +1,8 @@
 package com.the.bug.one.job;
 
 import com.the.bug.one.config.PropertyConfig;
-import com.the.bug.one.util.Utility;
 import org.apache.log4j.Logger;
+import org.quartz.JobKey;
 import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
 
@@ -18,16 +18,20 @@ public class BackUpTask {
         try {
             var sf = new StdSchedulerFactory();
             var scheduler = sf.getScheduler();
-            var job = newJob(BackUpJob.class)
-                    .withIdentity("backup", "groupOne")
+            var jobKey = JobKey.jobKey("backup", "groupOne");
+            var jobDetail = newJob(BackUpJob.class)
+                    .withIdentity(jobKey)
                     .build();
             var trigger = newTrigger()
                     .withIdentity("triggerOne", "groupOne")
                     .withSchedule(cronSchedule(CONFIG.getCronExpression()))
                     .build();
-            scheduler.scheduleJob(job, trigger);
-            scheduler.start();
+
             LOGGER.info("Cron job started");
+            scheduler.scheduleJob(jobDetail, trigger);
+            scheduler.start();
+            LOGGER.info("Executing first job");
+            scheduler.triggerJob(jobKey);
         } catch (SchedulerException e) {
             LOGGER.error(e.getMessage());
         }
